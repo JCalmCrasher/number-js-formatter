447b1ba4324d05dded46bc0c0e0a5e94356ad830
JoshX [MacBookPro] <joshua.osagie01@gmail.com>
2022-03-22 18:39:41 +0100

    chore: add dependencies

--- src/friendly-format.ts
+++ src/friendly-format.ts
@@ -1,13 +1,19 @@
-import lookup from "./number-lookup";
+import lookup from './number-lookup';
 
-export function friendlyFormat(number: number, digits: number = 0) {
-    const lookupNumbers = lookup;
+export function friendlyFormat(number: number, digits = 0) {
+  const lookupNumbers = lookup;
 
-    const regex = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  const regex = /\.0+$|(\.[0-9]*[1-9])0+$/;
 
-    let result = lookupNumbers.slice().reverse().find(function (item) {
-        return number >= item.value;
+  const result = lookupNumbers
+    .slice()
+    .reverse()
+    .find(function (item) {
+      return number >= item.value;
     });
 
-    return result ? (number / result.value).toFixed(digits).replace(regex, "$1") + result.symbol : "0";
+  return result
+    ? (number / result.value).toFixed(digits).replace(regex, '$1') +
+        result.symbol
+    : '0';
 }
--- src/number-lookup.ts
+++ src/number-lookup.ts
@@ -1,9 +1,9 @@
 const lookup = [
-    { value: 1, symbol: "" },
-    { value: 1e3, symbol: "k" },
-    { value: 1e6, symbol: "m" },
-    { value: 1e9, symbol: "bn" },
-    { value: 1e12, symbol: "tn" },
+  { value: 1, symbol: '' },
+  { value: 1e3, symbol: 'k' },
+  { value: 1e6, symbol: 'm' },
+  { value: 1e9, symbol: 'bn' },
+  { value: 1e12, symbol: 'tn' }
 ];
 
 export default lookup;
