abf91b16ffa5ec4bcf433836454b9d63358e4d02
joshx <joshua.osagie01@gmail.com>
2022-08-12 11:41:07 +0100

    test: snapshot values

--- __tests__/format.test.ts
+++ __tests__/format.test.ts
@@ -4,18 +4,18 @@ import { describe, expect, it } from 'vitest';
 
 describe('format', () => {
   it('should format thousand', () => {
-    expect(friendlyFormat(THOUSAND)).toBe('1k');
+    expect(friendlyFormat(THOUSAND)).toMatchSnapshot();
   });
 
   it('should format million', () => {
-    expect(friendlyFormat(MILLION)).toBe('1m');
+    expect(friendlyFormat(MILLION)).toMatchSnapshot();
   });
 
   it('should format billion', () => {
-    expect(friendlyFormat(BILLION)).toBe('1bn');
+    expect(friendlyFormat(BILLION)).toMatchSnapshot();
   });
 
   it('should format trillion', () => {
-    expect(friendlyFormat(TRILLION)).toBe('1tn');
+    expect(friendlyFormat(TRILLION)).toMatchSnapshot();
   });
 });
---
+++ src/numbers.ts
@@ -0,0 +1,4 @@
+export const THOUSAND = 1000;
+export const MILLION = 1000_000;
+export const BILLION = 1000_000_000;
+export const TRILLION = 1000_000_000_000;
