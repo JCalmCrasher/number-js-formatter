bbc44e120df34e05cade68d0f2bc5f3affa174b4
joshx <joshua.osagie01@gmail.com>
2022-08-12 10:14:53 +0100

    migrate test to `vitest`

--- __tests__/format.test.ts
+++ __tests__/format.test.ts
@@ -1,3 +1,10 @@
-test('format thousand', () => {
-  //
+import { describe, expect, test } from 'vitest';
+import { friendlyFormat } from '@/friendly-format';
+
+describe('format', () => {
+  test('format thousand', () => {
+    const numb1 = 1000000;
+
+    expect(friendlyFormat(numb1)).toBe('1m');
+  });
 });
